319be9b9da6825cd3487ed81859e50090b039638
vagrant <vagrant@precise64.(none)>
2015-03-04 22:51:06 +0000

    New script/command for creatign a new roots/bedrock site.

---
+++ scripts/create-bedrock-project.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+webrootdir=/vagrant/www
+apacheconfdir=/vagrant/scripts/apacheconf
+apachedir=/etc/apache2/sites-available
+
+if [ ! -d "$webrootdir"/$1 ]; then
+	# Create the project
+	composer create-project roots/bedrock "$webrootdir"/$1
+
+	# Copy example conf file to the the new sites conf file
+	cp "$apacheconfdir"/example_bedrock.conf "$apacheconfdir"/$1.conf
+
+	# Replace all references to example with the new site name
+	perl -pi -e "s/example/$1/g" "$apacheconfdir"/$1.conf
+
+	sudo cp "$apacheconfdir"/$1.conf "$apachedir"/
+
+	sudo a2ensite $1
+
+	sudo service apache2 reload
+fi
