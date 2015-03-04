7a4d347a6bb54c1cbe69ab8189802404b9521583
Andreas Färnstrand <andreas@farnstranddev.se>
2015-03-04 10:52:08 +0100

    Added new features in bootstrap. Also added an example virtualhost file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 webdirectory=www
 scriptsdirectory=scripts
 dbdirectory=db
 
 # Create the forlder for the websites
-if [ ! -d "$webdirectory" ]; then
-	mkdir $webdirectory
+if [ ! -d /vagrant/"$webdirectory" ]; then
+	mkdir /vagrant/"$webdirectory"
 fi
 
 # Create the forlder for custom scripts
-if [ ! -d "$scriptsdirectory" ]; then
-	mkdir $scriptsdirectory
+if [ ! -d /vagrant/"$scriptsdirectory" ]; then
+	mkdir /vagrant/"$scriptsdirectory"
+fi
+
+# Create the forlder for custom scripts
+if [ ! -d /vagrant/"$scriptsdirectory"/apacheconf ]; then
+	mkdir /vagrant/"$scriptsdirectory"/apacheconf
 fi
 
 # Create the folder for db dumps
-if [ ! -d "$dbdirectory" ]; then
-	mkdir $dbdirectory
+if [ ! -d /vagrant/"$dbdirectory" ]; then
+	mkdir /vagrant/"$dbdirectory"
+fi
+
+# Copy all virtualhosts to apache's sites directory and activate the site
+if [ -d /vagrant/"$scriptsdirectory"/apacheconf ]; then
+	for filename in /vagrant/"$scriptsdirectory"/apacheconf/*.conf; do
+    	sudo cp "$filename" /etc/apache2/sites-available/
+    	a2ensite "$filename"
+	done
 fi
